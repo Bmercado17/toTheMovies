656f97ecf4c9999d5d6a135d09cd0e9186cac483
Bmercado17 <brnrdsnchz17@gmail.com>
2020-05-13 06:25:42 -0700

    categories feature

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -5,11 +5,30 @@
 //     };
     
 // })();
+
+
 var gMovie = {};
+gMovie.database = [];
 gMovie.init = function(){
     gMovie.filterSlider();
 
 }
+
+gMovie.loadAssets = function(){
+
+    $.getJSON("/db/movies.json",function(data){
+       
+        gMovie.database = data;
+        gMovie.init();
+        gMovie.getTypes ();
+    });
+}
+gMovie.init = function(){
+    gMovie.filterSlider();
+}
+
+
+
 gMovie.filterSlider = function(){
     
     $(".filter.open").on("click", function(){
@@ -17,10 +36,10 @@ gMovie.filterSlider = function(){
         $(".filter-container").slideToggle(400, function(){
             let btn = $(this).prev();
 
-            if(btn.hasClass("active")){
+            if(btn.hasClass("active")){//remove the class of active to the button so it drops down d menu
                 $(".filter.open").find(".btn-title").text("Filter By");
                 btn.removeClass("active");
-            }else{
+            }else{ //add the class of active to close the filter button
                 $(".filter.open").find(".btn-title").text("Close");
                 btn.addClass("active");
             }
@@ -29,4 +48,18 @@ gMovie.filterSlider = function(){
         })
     })
 }
-gMovie.init();
+gMovie.getTypes = function(){
+
+    var types = [];
+
+    $.each(gMovie.database, function(index, elem){
+        var typeValue = gMovie.database[index].type;
+        types.push(typeValue);
+        $('#categories').append('<option value=" '+ typeValue + '"> '+ typeValue + 
+        '</option>')
+
+    })
+
+};
+
+gMovie.loadAssets();
